b9d37a1de7b6f8bb8f4b2597f140128c41e6da64
saira-uwc <saira@unitedwecare.com>
2025-12-10 18:15:40 +0530

    Refactor home page redirection tests to include setup and detailed navigation steps

--- tests/Expert Dashboard/home-page-redirections.spec.ts
+++ tests/Expert Dashboard/home-page-redirections.spec.ts
@@ -1,22 +1,44 @@
-// import { test, expect } from '../../helpers/auto-fixture.js';
+import { test, expect } from '../../helpers/auto-fixture.js';
 
-// const BASE = process.env.BASE_URL || 'https://asksam.com.au/';
+const BASE = process.env.BASE_URL || 'https://asksam.com.au/';
 
-// test.beforeEach(async ({ page }) => {
-//   // Ensure clean state before each test
-//   await page.context().clearCookies();
-//   await page.addInitScript(() => { 
-//     localStorage.clear(); 
-//     sessionStorage.clear(); });
-// });
+test.beforeEach(async ({ page }) => {
+  // Ensure clean state before each test
+  await page.context().clearCookies();
+  await page.addInitScript(() => { 
+    localStorage.clear(); 
+    sessionStorage.clear(); });
+});
 
-// test('', async ({ page }) => {
-//   // Always start from sign-in page
-//   await page.goto(BASE);
+test('Home page redirections', async ({ page }) => {
+  // Always start from sign-in page
+  await page.goto(BASE);
 
-//   // --- YOUR RECORDED CODE START ---
-  
+  // --- YOUR RECORDED CODE START ---
+  await page.goto('https://asksam.com.au/');
+  await page.locator('img').nth(4).click();
+  await page.getByRole('link', { name: 'Sign up / Log in', exact: true }).click();
+  await page.getByRole('button', { name: 'Log in here' }).click();
+  await page.getByText('Clinician').click();
+  await page.getByRole('button', { name: 'Continue' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('testing_clinician_aus+clerk_test@tmail.com');
+  await page.getByRole('button', { name: 'Continue' }).click();
+  await page.getByRole('textbox', { name: 'Enter verification code' }).fill('424242');
+  await page.goto('https://copilot.asksam.com.au/clinical/home');
+  await page.locator('img').first().click();
+  const page1Promise = page.waitForEvent('popup');
+  await page.getByRole('link', { name: 'Appointment Dashboard' }).click();
+  const page1 = await page1Promise;
+  await page1.getByRole('link', { name: 'appointments', exact: true }).click();
+  await page1.getByRole('heading', { name: 'Appointments' }).click();
+  await page1.getByRole('link', { name: 'chat', exact: true }).click();
+  await page1.getByRole('heading', { name: 'Patients' }).click();
+  await page1.getByRole('link', { name: 'notifications', exact: true }).click();
+  await page1.getByRole('heading', { name: 'Notifications' }).click();
+  await page1.getByRole('button', { name: 'Open user menu' }).click();
+  await page1.getByRole('menuitem', { name: 'Sign out' }).click();
 
 
-//   // --- YOUR RECORDED CODE END ---
-// });
+  // --- YOUR RECORDED CODE END ---
+});
